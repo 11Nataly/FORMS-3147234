e0a094dae120367cfa74ae30eca0a16840a049ec
nat <19nat.9295@gmail.com>
2025-08-02 10:53:06 -0500

    subir modales

---
+++ main.js
@@ -0,0 +1,15 @@
+import { hideModal, showModal } from './modal';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('survey-form');
+  const closeBtn = document.getElementById('close-modal');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    showModal();
+  });
+
+  closeBtn.addEventListener('click', () => {
+    hideModal();
+  });
+});
