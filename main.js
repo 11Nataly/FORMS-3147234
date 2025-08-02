d954015ff5a3864074aacca6c6e037ecf537837a
nat <19nat.9295@gmail.com>
2025-08-02 10:55:58 -0500

    agregar el modal del formulario

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { hideModal, showModal } from './modal';
+import { hideModal, showModal } from './modal.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('survey-form');
